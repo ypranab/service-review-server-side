c66e89ade900f94e9551119f0c4fc66bd2048fff
ypranab <ypranab11@gmail.com>
2022-12-20 23:01:49 +0600

    env added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://mongoITservices:xyCZGb5duZHeU0gS@cluster0.nslo89v.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nslo89v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
 async function run() {
