424044fba1d9157a1fc479358c5b247f396c7f72
ypranab <ypranab11@gmail.com>
2022-12-20 23:36:29 +0600

    dotenv config added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const app = express();
+
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nslo89v.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nslo89v.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 console.log(uri)
 async function run() {
